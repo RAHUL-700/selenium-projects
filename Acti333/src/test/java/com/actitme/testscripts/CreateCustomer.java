43ea71da194c1c3f70e176549355423a401835d2
satya <satya@LAPTOP-9B3C4EKN>
2023-04-18 12:16:47 +0530

    fatar

---
+++ Acti333/src/test/java/com/actitme/testscripts/CreateCustomer.java
@@ -0,0 +1,13 @@
+package com.actitme.testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCustomer {
+
+	@Test
+	public void create() {
+		Reporter.log("CreateCustomer",true);
+	}
+	
+}
